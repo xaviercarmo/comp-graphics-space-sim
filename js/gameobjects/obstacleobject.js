673cd75eb2f219a43737f103570539eecd06c3a9
Kevin L <ReMashed@users.noreply.github.com>
2020-04-24 01:59:15 +1000

    Random Spawn w Random Size
    - Object now spawns within a decent range (0-100)
    - Object size is now random between 5-20 per dimensions.

--- js/gameobjects/obstacleobject.js
+++ js/gameobjects/obstacleobject.js
@@ -6,26 +6,31 @@ import GameObject from '../gameobject.js';
 class ObstacleObject extends GameObject {
 
   //characteristics
-  /*
-  #height = 1;
-  #width = 1;
-  */
-
   //no parameter as object is being created here.
   constructor(){
     super(obst);
 
     //create basic object
-    var geo = new THREE.BoxGeometry(11, 11, 11);
+    //randomise size
+    var w, h , d;
+    w = THREE.MathUtils.randInt(5, 20);
+    h = THREE.MathUtils.randInt(5, 20);
+    d = THREE.MathUtils.randInt(5, 20);
+    var geo = new THREE.BoxGeometry( w, h , d);
     var mat = new THREE.MeshBasicMaterial( { color:	0xff0000 });
     var obst = new THREE.Mesh(geo, mat);
 
     //random spawn
     var x, y, z;
-    x=y=z=Math.random();
-    console.log(x, y, z);
-    obst.position.set(x, y, z);
-    
+    //x=y=z=Math.random();
+
+    //Random int per axis
+    x = THREE.MathUtils.randInt(0, 100);
+    y = THREE.MathUtils.randInt(0, 100);
+    z = THREE.MathUtils.randInt(0, 100);
+    console.log( x, y , z);
+    obst.position.set( x, y, z);
+
     //add object to scene
     window.GameHandler.Scene.add(obst);
   }
@@ -33,6 +38,7 @@ class ObstacleObject extends GameObject {
   Main(dt){
     super.Main();
   }
+
 }
 
 export default ObstacleObject;
