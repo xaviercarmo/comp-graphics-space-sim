9e1abcfb3c4d3bbd46c003f5338f521f99486c0c
Kevin L <ReMashed@users.noreply.github.com>
2020-04-30 17:03:40 +1000

    Cube to Icosahedron object
    - changed cube to Icosahedron

--- js/gameobjects/obstacleobject.js
+++ js/gameobjects/obstacleobject.js
@@ -2,21 +2,24 @@ import * as THREE from '../../libraries/three.module.js';
 import * as UTILS from '../utils.js';
 
 import GameObject from '../gameobject.js';
+import GameHandler from '../gamehandler.js';
 
 class ObstacleObject extends GameObject {
 
   //characteristics
+  xpos;
+  ypos;
   //no parameter as object is being created here.
   constructor(){
-    super(obst);
+    super(obst); 
 
     //create basic object
     //randomise size
-    var w, h , d;
-    w = THREE.MathUtils.randInt(5, 20);
-    h = THREE.MathUtils.randInt(5, 20);
-    d = THREE.MathUtils.randInt(5, 20);
-    var geo = new THREE.BoxGeometry( w, h , d);
+    var det, rad;
+    det = THREE.MathUtils.randInt(1, 3);
+    rad = THREE.MathUtils.randInt(1, 20);
+    
+    var geo = new THREE.IcosahedronGeometry(rad, det);
     var mat = new THREE.MeshBasicMaterial( { color:	0xff0000 });
     var obst = new THREE.Mesh(geo, mat);
 
@@ -28,9 +31,10 @@ class ObstacleObject extends GameObject {
     x = THREE.MathUtils.randInt(0, 100);
     y = THREE.MathUtils.randInt(0, 100);
     z = THREE.MathUtils.randInt(0, 100);
+    console.log(this._mass);
     console.log( x, y , z);
-    obst.position.set( x, y, z);
-
+    obst.position.set( x, y, z); 
+   
     //add object to scene
     window.GameHandler.Scene.add(obst);
   }
