4a3382086146c6bfd74d5d4e176f7b76ed442ba4
rohan678 <63397085+rohan678@users.noreply.github.com>
2020-06-10 23:55:58 +1000

    New health boost changes

--- js/gameobjects/healthcapsule.js
+++ js/gameobjects/healthcapsule.js
@@ -11,9 +11,31 @@ class HealthCapsule extends GameObject {
         this.#gltfObject = window.GameHandler.AssetHandler.LoadedAssets.health_capsule;
 
         this._mainObject.traverse(child => {
-            // apply materials here based on child.name (e.g. if child.name starts with Torus, add shiny phong material)
+          // apply materials here based on child.name (e.g. if child.name starts with Torus, add shiny phong material)
+          var lidmaterial = new THREE.MeshPhongMaterial({ color: 0xCC3333 });
+          var innercylindermat = new THREE.MeshPhongMaterial({ color: 0x00FF66});
+          var ringmat = new THREE.MeshPhongMaterial({ color: 0xFFFF99});
+
+
+            if(child.name == 'lidtop' || child.name == 'lidbot') {
+                child.material = lidmaterial;
+
+            };
+            if(child.name == 'innercylinder') {
+                child.material = innercylindermat;
+                child.layers.enable(window.GameHandler.RenderLayers.BLOOM_STATIC_HIGH);
+            };
+            if(child.name == 'ring1' || child.name == 'ring2' || child.name == 'ring3') {
+              child.material = ringmat;
+              child.layers.enable(window.GameHandler.RenderLayers.BLOOM_STATIC);
+            };
+            if(child.name == 'crosstop1' || child.name == 'crosstop2' || child.name == 'crossbot1' || child.name == 'crossbot2') {
+              child.material = ringmat;
+              child.layers.enable(window.GameHandler.RenderLayers.BLOOM_STATIC);
+
+          };
         })
-        
+
         //scale down the capsule
         this._mainObject.scale.set(5, 5, 5); //_mainObject == this.#gltfObject.scene, because of the parent's constructor
 
